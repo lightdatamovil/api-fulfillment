e39bab91b568d2408af3dc1cce430aec16828fd4
Agustt123 <agustintracheskyoficial@gmail.com>
2025-05-16 10:54:17 -0300

    CRUD INSUMOS

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use("/clienteCuenta", require("./route/route-clienteCuenta"));
 app.use("/atributo", require("./route/route-atributo"));
 app.use("/stock", require("./route/route-stock"));
 app.use("/ordenes", require("./route/route-ordenes"));
-
+app.use("/insumo", require("./route/route-insumo"));
 app.get("/", (req, res) => {
   res.status(200).json({
     estado: true,
---
+++ route/route-insumo.js
@@ -0,0 +1,162 @@
+const express = require("express");
+const insumo = express.Router();
+
+const {
+  redisClient,
+  getConnection,
+  getCompanyById,
+  getConnectionLocal,
+} = require("../dbconfig");
+const Usuario = require("../controller/usuario/usuario");
+const verificarToken = require("../middleware/token");
+const Insumo = require("../controller/insumo/insumos");
+
+insumo.post("/postInsumo", verificarToken, async (req, res) => {
+  const data = req.body;
+  const connection = await getConnectionLocal(data.idEmpresa);
+
+  try {
+    // Crear nuevo producto
+    const insumo = new Insumo(
+      data.did || "",
+      data.nombre,
+      data.didCliente,
+      data.codigo,
+      data.habilitado,
+      data.quien,
+      data.superado || 0,
+      data.elim || 0,
+      connection
+    );
+    const insumoResult = await insumo.insert();
+
+    if (insumoResult.estado === false) {
+      return res.status(400).json({
+        estado: false,
+        message: insumoResult.message || insumoResult,
+      });
+    }
+    const insumoId = insumoResult.insertId;
+
+    return res.status(200).json({
+      estado: true,
+      message: "Insumo creado correctamente",
+      didInsumo: insumoId,
+    });
+  } catch (error) {
+    console.error("Error durante la operación:", error);
+    return res.status(500).json({
+      estado: false,
+      error: -1,
+      message: error.message || error,
+    });
+  } finally {
+    connection.end();
+  }
+});
+
+insumo.post("/getInsumos", verificarToken, async (req, res) => {
+  const data = req.body;
+  const connection = await getConnectionLocal(data.idEmpresa);
+  const { page, limit, nombre, habilitado, codigo, did, didCliente } = data;
+  const insumo = new Insumo();
+  try {
+    const response = await insumo.getInsumos(connection, {
+      page: page || 1,
+      limit: limit || 10,
+      nombre: nombre || "",
+      habilitado: habilitado || "",
+      codigo: codigo || "",
+      did: did || "",
+      didCliente: didCliente || "",
+    });
+    if (response.estado === false) {
+      return res.status(400).json({
+        estado: false,
+        message: response.message || response,
+      });
+    }
+
+    return res.status(200).json({
+      estado: true,
+      totalRegistros: response["totalRegistros"],
+      totalPaginas: response["totalPaginas"],
+      pagina: response["pagina"],
+      cantidad: response["cantidad"],
+      data: response["data"],
+    });
+  } catch (error) {
+    console.error("Error durante la operación:", error);
+    return res.status(500).json({
+      estado: false,
+      error: -1,
+      message: error.message || error,
+    });
+  } finally {
+    connection.end();
+  }
+});
+
+insumo.post("/getInsumosById", verificarToken, async (req, res) => {
+  const data = req.body;
+  const connection = await getConnectionLocal(data.idEmpresa);
+  const insumo = new Insumo();
+
+  try {
+    const response = await insumo.getInsumosById(connection, data.did);
+
+    if (response.estado === false) {
+      return res.status(400).json({
+        estado: false,
+        message: response.message || response,
+      });
+    }
+
+    return res.status(200).json({
+      estado: true,
+      data: response[0],
+    });
+  } catch (error) {
+    console.error("Error durante la operación:", error);
+    return res.status(500).json({
+      estado: false,
+      error: -1,
+      message: error.message || error,
+    });
+  } finally {
+    connection.end();
+  }
+});
+
+insumo.post("/deleteInsumo", async (req, res) => {
+  const data = req.body;
+  const connection = await getConnectionLocal(data.idEmpresa);
+
+  try {
+    const insumo = new Insumo();
+    const response = await insumo.delete(connection, data.did);
+
+    return res.status(200).json({
+      estado: response.estado !== undefined ? response.estado : false,
+      message: response.message || response,
+    });
+  } catch (error) {
+    console.error("Error durante la operación:", error);
+    return res.status(500).json({
+      estado: false,
+      error: -1,
+      message: error.message || error,
+    });
+  } finally {
+    connection.end();
+  }
+});
+
+insumo.get("/", async (req, res) => {
+  res.status(200).json({
+    estado: true,
+    mesanje: "Hola chris",
+  });
+});
+
+module.exports = insumo;
