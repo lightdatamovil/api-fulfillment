405d883b11d0f61ea259359896502252e0880f86
Agustt123 <agustintracheskyoficial@gmail.com>
2025-05-28 17:17:43 -0300

    dssda

--- controller/usuario/usuario.js
+++ controller/usuario/usuario.js
@@ -112,11 +112,14 @@ class Usuario {
       ]);
 
       if (results.length > 0) {
-        if (!this.pass || this.pass.trim() === "") {
+        if (!this.pass || this.pass.trim() == "") {
           // Mantener la contraseña antigua
           console.log("Mantener la contraseña antigua");
 
+          console.log(results[0].pass);
+
           this.pass = results[0].pass;
+          console.log(this.pass);
         }
         const queryUpdate = "UPDATE usuarios SET superado = 1 WHERE did = ?";
         await executeQuery(connection, queryUpdate, [this.did]);
