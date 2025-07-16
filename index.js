f549ccf95fe352ffaf522b9afb633739794ef1fb
Agustt123 <agustintracheskyoficial@gmail.com>
2025-07-16 14:42:19 -0300

    cambios en ordenById

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use("/usuario", require("./route/route-usuario"));
 app.use("/clienteCuenta", require("./route/route-clienteCuenta"));
 app.use("/atributo", require("./route/route-atributo"));
 app.use("/stock", require("./route/route-stock"));
-app.use("/ordenes", require("./route/route-ordenes"));
+app.use("/orden", require("./route/route-ordenes"));
 app.use("/insumo", require("./route/route-insumo"));
 app.use("/publicaciones", require("./route/route-publicaciones"));
 app.get("/", (req, res) => {
