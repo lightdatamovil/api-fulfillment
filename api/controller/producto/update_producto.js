88929b4697b768eeab4a2e4689b38ba6b2f27db5
Gonn01 <gonzalomiguelrigoni@gmail.com>
2025-11-04 12:40:29 -0300

    Refactor file handling in updateProducto function to simplify file insertion logic

--- api/controller/producto/update_producto.js
+++ api/controller/producto/update_producto.js
@@ -45,7 +45,7 @@ export async function updateProducto(dbConnection, req) {
   const curr = currRows[0];
 
   //files
-  let filesInsert = [];
+  let filesInsert;
   if (files == null) {
     filesInsert = curr.files; //no hay cambios
   } else if (files.length === 0) {
@@ -63,7 +63,7 @@ export async function updateProducto(dbConnection, req) {
       { headers: { "Content-Type": "application/json" } }
     );
     console.log('urlResponse:', urlResponse.data);
-    filesInsert.push(urlResponse.data.file.url);
+    filesInsert = urlResponse.data.file.url;
 
   }
 
@@ -79,7 +79,7 @@ export async function updateProducto(dbConnection, req) {
     alto,
     ancho,
     profundo,
-    filesInsert,
+    imagen: filesInsert,
     sku,
     ean,
     did_curva,
