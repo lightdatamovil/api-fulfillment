d83f2d361a62e2a572e7e66dd6ab5ff2c964bf5e
denu <nissgonzalezm@gmail.com>
2025-11-11 10:28:45 -0300

    refactor: remove console logs from addStock function for cleaner code

--- api/controller/stock/add_stock.js
+++ api/controller/stock/add_stock.js
@@ -21,7 +21,7 @@ export async function addStock({ db, req }) {
         where: { did: did_producto },
     });
 
-    console.log('Producto verificacion:', productoVerificacion);
+
 
     //verifico cuanto hay en la ultima fila de stock
     const stockActual = await LightdataORM.select({
@@ -30,12 +30,12 @@ export async function addStock({ db, req }) {
         where: { did_producto_combinacion: did_combinacion },
     });
 
-    console.log('Ultimo stock encontrado:', stockActual[0].stock);
+
 
     //sumo cantidad con la nueva cantidad
     const nuevaCantidad = (stockActual[0]?.stock || 0) + Number(cantidad);
 
-    console.log('Nueva cantidad calculada:', nuevaCantidad);
+
 
     // actualizo la tabla stock_productos con la nueva cantidad
     const [didUpdateResult] = await LightdataORM.update({
@@ -51,12 +51,12 @@ export async function addStock({ db, req }) {
         }
     });
 
-    console.log('Resultado de la actualización del stock:', didUpdateResult);
+
 
     // VERIFICAR SI tiene_ie es 1
     // const ie = productoVerificacion[0].data_ie;
     if (productoVerificacion[0].tiene_ie == 1) {
-        console.log('El producto requiere identificadores especiales.');
+
         if (!Array.isArray(identificadores_especiales) || identificadores_especiales.length === 0) {
             throw new CustomException({
                 title: "Identificadores especiales requeridos",
@@ -66,7 +66,7 @@ export async function addStock({ db, req }) {
 
         // los tengo que agrupar en un json para guardar en data_ie asi {1:148, 2:2025/10/6} que esta en el array [] como objetos {did:1, valor:asjda} identificadores especiales
 
-        const data_ie = identificadores_especiales.reduce((acc, item, i) => {
+        const data_ie = identificadores_especiales.reduce((acc, item) => {
             const { did, valor } = item;
             acc[did] = valor;
             return acc;
@@ -75,7 +75,7 @@ export async function addStock({ db, req }) {
 
         // hashear json data_ie y guardar en hash 256
         const hash = createHash('sha256').update(JSON.stringify(data_ie)).digest('hex');
-        console.log('Hash generado:', hash);
+
 
 
         //armo para insertar
@@ -88,9 +88,7 @@ export async function addStock({ db, req }) {
             did_producto_variante_stock: didUpdateResult,
             hash: hash
         };
-        console.log('Detalle de stock a insertar:', stock_detalle);
 
-        console.log('DATA IE a guardar:', data_ie);
 
         // inserto en stock_producto_detalle : 1 traigo la ultima fila e inserto una nueva y supero la enterior updatear did
 
