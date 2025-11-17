decf1c054d326f6f79db8090a059ed6f15ded829
Agustt123 <agustintracheskyoficial@gmail.com>
2025-11-17 15:39:07 -0300

    dsadsad

--- rabbit/functions/obtenerCliente.js
+++ rabbit/functions/obtenerCliente.js
@@ -4,7 +4,7 @@ export async function obtenerClienteCuenta(db, ml_id_vendedor) {
 
     const rows = await executeQuery({
         db,
-        query: `SELECT * FROM clientes WHERE ml_id_vendedor = ? AND elim = 0 LIMIT 1`,
+        query: `SELECT * FROM clientes_cuentas WHERE ml_id_vendedor = ? AND elim = 0 LIMIT 1`,
         values: [ml_id_vendedor]
     });
 
